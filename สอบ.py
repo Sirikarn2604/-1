db858a36e2c4e2e53d36db4707d29653fb7c5158
Sirikarn2604 <post.sirikarn@gmail.com>
2024-12-23 14:55:11 +0700

    Add files via upload

---
+++ สอบ.py
@@ -0,0 +1,17 @@
+x = input()
+n = int(input())
+z = (x*n)
+print(z)
+
+midterm = float(input("")
+final = float(input("")
+print("Total:", midterm + final )
+print("Average:",toyal / 2)
+              
+ความกว้าง=float(input())
+ความยาว=float(input())
+ความลึก=float(input())
+ปริมาตร=ความกว้าง*ความยาว*ความลึก
+ต้องใช้เวลา=(ปริมาตร/0.25)
+print(ต้องใช้เวลา)
+
